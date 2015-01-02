a615a2df895be52ddd345594b9576266617bd4d2
atulsky88 <atulsky88@gmail.com>
2015-01-02 15:01:10 +0530

    first push

---
+++ SignalR/TicTacToeLib/UserDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.TicTacToeLib
+{
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
