d6f5a721bbcb581cfbe696f888ea2184fbf2f5c1
atulsky88 <atulsky88@gmail.com>
2015-01-05 12:04:39 +0530

    load table and call function on click of each row

--- SignalR/Scripts/TicTacToe/TicTacToeSignalR.js
+++ SignalR/Scripts/TicTacToe/TicTacToeSignalR.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     console.log("ready!");
     setScreen();
 
-    $('#onlineUserList').bootstrapTable({
-        data: getRows()
-    });
+    //$('#onlineUserList').bootstrapTable({
+    //    data: getRows()
+    //});
 
 
     //Declare a proxy to reference the hub. 
@@ -17,12 +17,11 @@ $(document).ready(function () {
     //Start Hub
 
     $.connection.hub.start().done(function () {
-        registerEvents(ticTacToeHub)
         console.log("connection.hub.start() : connected");
     }).fail(function () {
         alert("connection.hub.start() failed");
     });
-    getUserAndConnect();
+    //getUserAndConnect();
 
     $("#btn-save-user").click(function (e) {
         e.preventDefault();
@@ -30,56 +29,31 @@ $(document).ready(function () {
         if ($.trim(name) != '') {
             currentUserName = name;
             $("#userNameDialog").modal('hide');
+            registerEvents(ticTacToeHub);
         }
         else {
         }
-    })
-
-    
-    $('#onlineUserList').click(function (e) {
-        //$(this).hide();
-        e.preventDefault();
-        debugger
-        var $result = $('#events-result');
-
-        $('#events-table').bootstrapTable({
-        }).on('all.bs.table', function (e, name, args) {
-            console.log('Event:', name, ', data:', args);
-        }).on('click-row.table', function (e, row, $element) {
-            $result.text('Event: click-row.bs.table, data: ' + JSON.stringify(row));
-        }).on('dbl-click-row.bs.table', function (e, row, $element) {
-            $result.text('Event: dbl-click-row.bs.table, data: ' + JSON.stringify(row));
-        }).on('sort.bs.table', function (e, name, order) {
-            $result.text('Event: sort.bs.table, data: ' + name + ', ' + order);
-        }).on('check.bs.table', function (e, row) {
-            $result.text('Event: check.bs.table, data: ' + JSON.stringify(row));
-        }).on('uncheck.bs.table', function (e, row) {
-            $result.text('Event: uncheck.bs.table, data: ' + JSON.stringify(row));
-        }).on('check-all.bs.table', function (e) {
-            $result.text('Event: check-all.bs.table');
-        }).on('uncheck-all.bs.table', function (e) {
-            $result.text('Event: uncheck-all.bs.table');
-        }).on('load-success.bs.table', function (e, data) {
-            $result.text('Event: load-success.bs.table');
-        }).on('load-error.bs.table', function (e, status) {
-            $result.text('Event: load-error.bs.table, data: ' + status);
-        }).on('column-switch.bs.table', function (e, field, checked) {
-            $result.text('Event: column-switch.bs.table, data: ' +
-                field + ', ' + checked);
-        }).on('page-change.bs.table', function (e, size, number) {
-            $result.text('Event: page-change.bs.table, data: ' + number + ', ' + size);
-        }).on('search.bs.table', function (e, text) {
-            $result.text('Event: search.bs.table, data: ' + text);
-        });
     });
 });
 
 function updateOnlineUserList(onlineUsers) {
     var users = humps.camelizeKeys(onlineUsers);
-
+    var $result = $('#events-result');
+  
     $('#onlineUserList').bootstrapTable({
         data: users
+    }).on('all.bs.table', function (e, name, args) {
+        console.log('Event:', name, ', data:', args);
+    }).on('click-row.bs.table', function (e, row, $element) {
+        $result.text('Event: click-row.bs.table, data: ' + JSON.stringify(row));
+    }).on('dbl-click-row.bs.table', function (e, row, $element) {
+        $result.text('Event: dbl-click-row.bs.table, data: ' + JSON.stringify(row));
+    }).on('load-success.bs.table', function (e, data) {
+        $result.text('Event: load-success.bs.table');
+    }).on('load-error.bs.table', function (e, status) {
+        $result.text('Event: load-error.bs.table, data: ' + status);
     });
+
 }
 
 function setScreen(isLogin) {
